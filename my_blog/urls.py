215552b12e1440047bc38e88547d1157f959dfa4
huhjhk <huhjhk@126.com>
2017-05-10 10:40:06 +0800

    django tutorial commit 0510

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -19,7 +19,7 @@ from article import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home),
-    url(r'^(?P<my_args>\d+)/$',views.detail, name='detail'),
+    url(r'^$', views.home,name='home'),
+    url(r'^(?P<id>\d+)/$',views.detail, name='detail'),
     url(r'^test/$', views.test),
 ]
